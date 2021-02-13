f860b5f03b2a40d10e626b6e1df504b62a0d8607
Serhat Seyren <sseyren@hotmail.com>
2021-02-13 20:13:43 +0300

    Updates

---
+++ precache-manifest.5c2433b6e3affa0652115d26838f2e19.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "434429ea53c03631a3697a7ffa35f155",
+    "url": "./index.html"
+  },
+  {
+    "revision": "40fe39eb1cfbd6e35dc8",
+    "url": "./static/css/main.6aafbd27.chunk.css"
+  },
+  {
+    "revision": "b359b26a7aa99229034b",
+    "url": "./static/js/2.94514647.chunk.js"
+  },
+  {
+    "revision": "e5067c2807f8c6eeca3663552efd09fb",
+    "url": "./static/js/2.94514647.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "40fe39eb1cfbd6e35dc8",
+    "url": "./static/js/main.610dc7ec.chunk.js"
+  },
+  {
+    "revision": "5a612039faf4d8a7dbca",
+    "url": "./static/js/runtime-main.2d1b3d35.js"
+  }
+]);
