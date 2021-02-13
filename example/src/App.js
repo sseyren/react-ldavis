14f5fc7567b7ca153fe0aa9c59c366d4e5209c69
Serhat Seyren <sseyren@hotmail.com>
2021-02-13 17:25:27 +0300

    data selection for example project

--- example/src/App.js
+++ example/src/App.js
@@ -1,12 +1,29 @@
-import React from 'react'
+import React, { useState } from 'react'
 
 import { LDAvis } from 'react-ldavis'
 import 'react-ldavis/dist/index.css'
 
 import LDAvisDataList from './data'
 
+const options = LDAvisDataList.map((_, i) => (
+  <option key={i} value={i}>{i}</option>
+))
+
 const App = () => {
-  return <LDAvis data={LDAvisDataList[0]} style={{textAlign: "center"}} />
+  const [currentData, setCurrentData] = useState(0);
+
+  return (
+    <div>
+      <label>Data: </label>
+      <select onChange={event => setCurrentData(event.target.value)}>
+        {options}
+      </select>
+      <LDAvis
+        data={LDAvisDataList[currentData]}
+        style={{ textAlign: "center" }}
+      />
+    </div>
+  )
 }
 
 export default App
